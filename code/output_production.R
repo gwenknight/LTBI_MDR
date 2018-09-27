ff6f7069fec968cb32cc4bda1601704d644c5201
GwenKnight <gwen.knight@github.com>
2018-09-27 18:19:27 +0100

    POP2035 and 2050 levels

--- code/output_production.R
+++ code/output_production.R
@@ -239,7 +239,7 @@ tt <- c(as.character("Global"),
 
 
 tb <- rbind(cbind(as.character(med.ltbir.g[,1]), 
-            rs1,rs2,rs3,rr1,rr2,rr3), tt)
+                  rs1,rs2,rs3,rr1,rr2,rr3), tt)
 colnames(tb) <- c("WHO region","best-lin ds","best-quad ds","best-quadc ds","best-lin dr","best-quad dr","best-quadc dr")
 write.csv(tb, "~/Dropbox/MDR/paper_output/compare_linquadquadc.csv")
 print(xtable(tb), include.rownames=FALSE)
@@ -291,41 +291,184 @@ dev.off()
 load('data/POP2035.Rdata')
 load('data/POP2050.Rdata')
 
-N2035 <- sum(POP2035[,"value"])
-POP2035$acatn <- 0:16
-POP2035$acat <- POP2035$acatn-4
-POP2035 <- subset(POP2035, POP2035$acat>=0)
-POP2035$pop35 = POP2035$value
-POP2035 <- POP2035[,c("iso3","acat","pop35")]
+cni <- read.csv("~/Dropbox/MDR/107_final_list_included_countries.csv", stringsAsFactors = FALSE)[,-1]
+elpd <- read.csv( "~/Dropbox/MDR/output/compare_models_elpd.csv")
+elpd_best <- c()
 
-N2050 <- sum(POP2050[,"value"])
-POP2050$acatn <- 0:16
-POP2050$acat <- POP2050$acatn-7
-POP2050 <- subset(POP2050, POP2050$acat>=0)
-POP2050$pop50 = POP2050$value
-POP2050 <- POP2050[,c("iso3","acat","pop50")]
+# Which level best for each country?
+for(i in 1:107){
+  cnn <- cni[i]
+  
+  w <- which(elpd$iso3 == cnn)
+  ee <- elpd[w,]
+  
+  if(length(which(w_data$iso3 == cnn)) < 3){ # Can't be quad
+    w2<-which(ee$X == "quad_loo")
+    ee <- ee[-w2,]
+  }
+  
+  max.elpd.level <- ee[which.max(ee$elpd_loo),"X"]
+  if(max.elpd.level == "lin_loo"){lev <- 1}
+  if(max.elpd.level == "quad_loo"){lev <- 2}
+  if(max.elpd.level == "quadc_loo"){lev <- 3}
+  
+  elpd_best <- rbind(elpd_best, c(cnn, lev))
+}
 
-fac <- 1e5*(0.15*1e-2)                  #reactivation rate
-
-## 2035
-# need iso, replicate, act, prev, g_whoregion 
-fruns <- merge(rundatar[,list(iso3,replicate,acat,P,g_whoregion)],
-               POP2035,by=c('iso3','acat'),all=TRUE)
-fruns <- fruns[!is.na(pop35),]       #ditch the dead
-tmp <- fruns[,list(nLTBI=sum(pop35*P)),by=replicate]
-tmp <- tmp[!is.na(replicate)]
-tmp[,list(mid=1e3*median(nLTBI),lo=1e3*lb(nLTBI),hi=1e3*ub(nLTBI))]
-
-
-fac*tmp[,list(mid=median(nLTBI)/N2035,lo=lb(nLTBI)/N2035,hi=ub(nLTBI)/N2035)]
-
-## 2050
-fruns <- merge(rundatar[,list(iso3,replicate,acat,P,g_whoregion)],
-               POP2050,by=c('iso3','acat'),all=TRUE)
-fruns <- fruns[!is.na(pop50),]       #ditch the dead
-tmp <- fruns[,list(nLTBI=sum(pop50*P)),by=replicate]
-tmp <- tmp[!is.na(replicate)]
-tmp[,list(mid=1e3*median(nLTBI),lo=1e3*lb(nLTBI),hi=1e3*ub(nLTBI))]
-
-fac*tmp[,list(mid=median(nLTBI)/N2050,lo=lb(nLTBI)/N2050,hi=ub(nLTBI)/N2050)]
-
+data_all <- c()
+for(ii in 1:4){
+  # Labels for each model
+  if(ii == 1){labl = "lin"}
+  if(ii == 2){labl = "quad"}
+  if(ii == 3){labl = "quadc"}
+  if(ii == 4){labl = "best"}
+  
+  datam <- as.data.frame(matrix(0,107*20000, 10))
+  
+  ### Gather data
+  if(ii < 4){
+    for(i in 1:107){
+      cnn <- cni[i]
+      d <- read.csv(paste0("~/Dropbox/MDR/cluster/",cnn,"_level2014_200_",labl,".csv"))[,-1]
+      d$age <- seq(1,100,1)
+      datam[(1 + (i-1)*20000):(i*20000),] <- d
+    }
+    colnames(datam) <- colnames(d)
+    data_all <- rbind(data_all, cbind(datam,ii)) # to find best
+  }
+  
+  if(ii == 4){
+    for(i in 1:107){
+      cnn <- cni[i]
+      w<-which(data_all$cn == cnn)
+      we <- which(elpd_best[,1] == cnn)
+      # which best?
+      lev <- elpd_best[we,2]
+      w2 <- which(data_all$ii == lev)
+      
+      cn_lev_best <- intersect(w,w2)
+      
+      datam[(1 + (i-1)*20000):(i*20000),] <- data_all[cn_lev_best,]
+    }
+  }
+  
+  
+  # 2035 population
+  N2035 <- sum(POP2035[,"value"])
+  POP2035$acatn <- 0:16
+  POP2035$acat <- POP2035$acatn-4
+  POP2035 <- subset(POP2035, POP2035$acat>=0)
+  POP2035$pop35 = POP2035$value
+  POP2035 <- POP2035[,c("iso3","acat","pop35")]
+  
+  # 2050 population 
+  N2050 <- sum(POP2050[,"value"])
+  POP2050$acatn <- 0:16
+  POP2050$acat <- POP2050$acatn-7
+  POP2050 <- subset(POP2050, POP2050$acat>=0)
+  POP2050$pop50 = POP2050$value
+  POP2050 <- POP2050[,c("iso3","acat","pop50")]
+  
+  #fac <- 1e5*(0.15*1e-2)                  #reactivation rate
+  
+  # Group by age categories
+  datam$acat <- c(rep(seq(0,15,1), each = 5), rep(16,20))
+  new_data <- datam[,c("pr_dr","pr_ds","mdr_rep","acat","popf")] %>% group_by(mdr_rep,acat,popf) %>% summarise_at(c("pr_dr","pr_ds"),funs(mean)) 
+  dim(new_data) # 107*200*17 = 363800
+  new_data$iso3 <- cni[new_data$popf]
+  
+  new_data2 <- merge(new_data,WHOkey[,c('iso3','g_whoregion')],by='iso3',all.x=TRUE)
+  
+  ## 2035
+  # need iso, replicate, act, prev, g_whoregion 
+  fruns <- merge(new_data2[,c("iso3","mdr_rep","acat","pr_dr","pr_ds","g_whoregion")],
+                 POP2035,by=c('iso3','acat'),all=TRUE)
+  fruns <- fruns[!is.na(fruns$mdr_rep),]     # ditch those countries not in the 107
+  fruns <- fruns[!is.na(fruns$pop35),]       # ditch the dead - only acat to 12 included
+  
+  fruns$pLTBIR <- 1000*fruns$pop35 * fruns$pr_dr # each number in POP2035 is the actual number divided by 1,000
+  fruns$pLTBIS <- 1000*fruns$pop35 * fruns$pr_ds
+  
+  dim(fruns) ## 200*107*13 = 278200
+  
+  # fac*tmp[,list(mid=median(nLTBI)/N2035,lo=lb(nLTBI)/N2035,hi=ub(nLTBI)/N2035)]
+  
+  fruns.total.35 <- aggregate(fruns[,c("pLTBIR","pLTBIS")], 
+                          list(fruns$mdr_rep), sum)
+  med.fruns.total.35 <- colwise(median)(fruns.total) / 1e6 # per million
+  ub.fruns.total.35 <- colwise(ub)(fruns.total) / 1e6
+  lb.fruns.total.35 <- colwise(lb)(fruns.total) / 1e6
+  
+  print(c(paste0(sprintf('%.1f',med.fruns.total.35$pLTBIR)," [",
+                         sprintf('%.1f',lb.fruns.total.35$pLTBIR),", ",
+                                 sprintf('%.1f',ub.fruns.total.35$pLTBIR),"]")))
+  print(c(paste0(sprintf('%.1f',med.fruns.total.35$pLTBIS)," [",
+                         sprintf('%.1f',lb.fruns.total.35$pLTBIS),", ",
+                                 sprintf('%.1f',ub.fruns.total.35$pLTBIS),"]")))
+  
+  
+  med.rate.35 <- (0.15*0.01) * med.fruns.total.35 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  ub.rate.35 <- (0.15*0.01) * ub.fruns.total.35 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  lb.rate.35 <- (0.15*0.01) * lb.fruns.total.35 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  
+  print(c(paste0(sprintf('%.1f',med.rate.35$pLTBIR)," [",
+                 sprintf('%.1f',lb.rate.35$pLTBIR),", ",
+                 sprintf('%.1f',ub.rate.35$pLTBIR),"]")))
+  print(c(paste0(sprintf('%.1f',med.rate.35$pLTBIS)," [",
+                 sprintf('%.1f',lb.rate.35$pLTBIS),", ",
+                 sprintf('%.1f',ub.rate.35$pLTBIS),"]")))
+  
+  
+  ## 2050
+  # need iso, replicate, act, prev, g_whoregion 
+  fruns <- merge(new_data2[,c("iso3","mdr_rep","acat","pr_dr","pr_ds","g_whoregion")],
+                 POP2050,by=c('iso3','acat'),all=TRUE)
+  fruns <- fruns[!is.na(fruns$mdr_rep),]     # ditch those countries not in the 107
+  fruns <- fruns[!is.na(fruns$pop50),]       # ditch the dead - only acat to 12 included
+  
+  fruns$pLTBIR <- 1000*fruns$pop50 * fruns$pr_dr # each number in POP2050 is the actual number divided by 1,000
+  fruns$pLTBIS <- 1000*fruns$pop50 * fruns$pr_ds
+  
+  dim(fruns) ## 200*107*13 = 278200
+  
+  # fac*tmp[,list(mid=median(nLTBI)/N2050,lo=lb(nLTBI)/N2050,hi=ub(nLTBI)/N2050)]
+  
+  fruns.total.50 <- aggregate(fruns[,c("pLTBIR","pLTBIS")], 
+                              list(fruns$mdr_rep), sum)
+  med.fruns.total.50 <- colwise(median)(fruns.total) / 1e6 # per million
+  ub.fruns.total.50 <- colwise(ub)(fruns.total) / 1e6
+  lb.fruns.total.50 <- colwise(lb)(fruns.total) / 1e6
+  
+  print(c(paste0(sprintf('%.1f',med.fruns.total.50$pLTBIR)," [",
+                 sprintf('%.1f',lb.fruns.total.50$pLTBIR),", ",
+                 sprintf('%.1f',ub.fruns.total.50$pLTBIR),"]")))
+  print(c(paste0(sprintf('%.1f',med.fruns.total.50$pLTBIS)," [",
+                 sprintf('%.1f',lb.fruns.total.50$pLTBIS),", ",
+                 sprintf('%.1f',ub.fruns.total.50$pLTBIS),"]")))
+  
+  
+  med.rate.50 <- (0.15*0.01) * med.fruns.total.50 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  ub.rate.50 <- (0.15*0.01) * ub.fruns.total.50 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  lb.rate.50 <- (0.15*0.01) * lb.fruns.total.50 * 1e6/1e5 # 0.15% reactivation x total in millions per 100,000
+  
+  print(c(paste0(sprintf('%.1f',med.rate.50$pLTBIR)," [",
+                 sprintf('%.1f',lb.rate.50$pLTBIR),", ",
+                 sprintf('%.1f',ub.rate.50$pLTBIR),"]")))
+  print(c(paste0(sprintf('%.1f',med.rate.50$pLTBIS)," [",
+                 sprintf('%.1f',lb.rate.50$pLTBIS),", ",
+                 sprintf('%.1f',ub.rate.50$pLTBIS),"]")))
+  
+  #### UP TO HERE
+  
+  
+  ## 2050
+  fruns <- merge(rundatar[,list(iso3,replicate,acat,P,g_whoregion)],
+                 POP2050,by=c('iso3','acat'),all=TRUE)
+  fruns <- fruns[!is.na(pop50),]       #ditch the dead
+  tmp <- fruns[,list(nLTBI=sum(pop50*P)),by=replicate]
+  tmp <- tmp[!is.na(replicate)]
+  tmp[,list(mid=1e3*median(nLTBI),lo=1e3*lb(nLTBI),hi=1e3*ub(nLTBI))]
+  
+  fac*tmp[,list(mid=median(nLTBI)/N2050,lo=lb(nLTBI)/N2050,hi=ub(nLTBI)/N2050)]
+  
+  
