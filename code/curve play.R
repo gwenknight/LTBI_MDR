cd4a17390279a66786353ec2e02005f878275af6
GwenKn <g.knight@imperial.ac.uk>
2018-10-04 20:14:23 +0100

    updated to years

--- code/curve play.R
+++ code/curve play.R
@@ -1,6 +1,7 @@
 #### Curve play
 
 library(ggplot2)
+library(boot)
 x <- seq(0,34, 1)
 
 # Time when MDR > 0
@@ -83,7 +84,7 @@ ggplot(curves, aes(x=year, y = y, group = rep, colour = rep)) + geom_line()
 ggsave("~/Dropbox/MDR/output/prior_curves_34.pdf")
 
 #### sequence
-tseq <- seq(5,44,5)
+tseq <- seq(1970,2014,5)
 bseq <- seq(0,0.05,0.01)
 rhoseq <- seq(0,1,0.1)
 
@@ -102,15 +103,17 @@ for(i in 1:length(tseq)){
       
       c <- rho * b / t_mdr
       
-      x <- matrix(0,1,45 - t_mdr)
-      x <- c(x,seq(1,t_mdr,1))
+      x <- seq(1950, 2020,1)
+      x <- x - t_mdr
       
-      y <- b*(x) - c*x^2 
+      y <- b*(x) - c*x^2
+      yi <- inv.logit(b*(x) - c*x^2) - 0.5
       
       rep = rep + 1
+      print(rep)
       
-      year = seq(1970,2014,1)
-      curves <- rbind(curves, cbind(rep,x, y,t_mdr,b,rho,c,year))
+      year = seq(1950,2020,1)
+      curves <- rbind(curves, cbind(rep,x, y, yi, t_mdr,b,rho,c,year))
       
     }
   }
@@ -120,4 +123,6 @@ curves <- as.data.frame(curves)
 
 ggplot(curves, aes(x=x, y = y, group = rep, colour = rep)) + geom_line() 
 ggplot(curves, aes(x=year, y = y, group = rep, colour = rep)) + geom_line() 
+ggplot(curves, aes(x=year, y = yi, group = rep, colour = rep)) + geom_line() +ggtitle("INVlog")
+ggplot(curves, aes(x=x, y = yi, group = rep, colour = rep)) + geom_line() +ggtitle("INVlog")
 ggsave("~/Dropbox/MDR/output/prior_curves_more.pdf")
