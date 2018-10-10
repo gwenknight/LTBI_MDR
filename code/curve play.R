8110ee195d0203e26a0a2f645b5597b0f580f5e4
GwenKnight <gwen.knight@github.com>
2018-10-10 19:03:57 +0100

    further playing

--- code/curve play.R
+++ code/curve play.R
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 library(boot)
+theme_set(theme_bw(base_size = 24))
 x <- seq(0,34, 1)
 
 # Time when MDR > 0
@@ -84,9 +85,9 @@ ggplot(curves, aes(x=year, y = y, group = rep, colour = rep)) + geom_line()
 ggsave("~/Dropbox/MDR/output/prior_curves_34.pdf")
 
 #### sequence
-tseq <- 1980 #seq(1970,2014,5)
-bseq <- 0.01 #seq(0,0.05,0.01)
-rhoseq <- seq(0,40,1)
+tseq <- 1970 #seq(1970,2014,5)
+bseq <- 0.001 #seq(0,0.05,0.01)
+rhoseq <- seq(-10,36,3)
 
 curves <- c()
 rep <- 0
@@ -103,7 +104,7 @@ for(i in 1:length(tseq)){
       
       c <- rho * b / t_mdr
       
-      x <- seq(1950, 2020,1)
+      x <- seq(1960, 2020,1)
       x <- x - t_mdr
       
       y <- b*(x) - c*x^2
@@ -112,7 +113,7 @@ for(i in 1:length(tseq)){
       rep = rep + 1
       print(rep)
       
-      year = seq(1950,2020,1)
+      year = seq(1960,2020,1)
       curves <- rbind(curves, cbind(rep,x, y, t_mdr,b,rho,c,year))
       
     }
@@ -121,7 +122,7 @@ for(i in 1:length(tseq)){
 
 curves <- as.data.frame(curves)
 
-ggplot(curves, aes(x=x, y = y, group = rep, colour = rep)) + geom_line() 
-ggplot(curves, aes(x=year, y = y, group = rep, colour = rep)) + geom_line() + scale_y_continuous(limits = c(0,3)) + 
-  geom_vline(xintercept = 2014)
-ggsave("~/Dropbox/MDR/output/prior_curves_more.pdf")
+#ggplot(curves, aes(x=x, y = y, group = rep, colour = rep)) + geom_line() 
+ggplot(curves, aes(x=year, y = y, group = rep, colour = factor(rho))) + geom_line(lwd = 2) + scale_y_continuous("Proportion of new TB cases\nthat are MDR-TB",limits = c(0,0.1)) + 
+  geom_vline(xintercept = 2014) + scale_color_discrete("r scalar") 
+ggsave("~/Dropbox/MDR/output/prior_curves_r_small.pdf")
