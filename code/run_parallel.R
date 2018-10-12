e3a6a9ee1c24bf6f183567d4bd3be707eb3c54ae
GwenKnight <gwen.knight@github.com>
2018-10-12 17:32:03 +0100

    run parallel with forking

--- code/run_parallel.R
+++ code/run_parallel.R
@@ -2,16 +2,29 @@
 
 library(foreach)
 library(doParallel)
+library(R.devices)
+
+
 
 # don't fill all cores
 no_cores <- detectCores() - 1
+
+
+#### SOCKETS
 cl<-makeCluster(no_cores)
 
 registerDoParallel(cl)
 
 source("~/Documents/LTBI_MDR/code/run_cohort_ltbi_mdr_inf_priors_parallel.R")
 
-save1 <- lapply(1:138, run_cohort_parallel)
+save1 <- lapply(86:138, run_cohort_parallel)
 
 
 stopCluster(cl)
+
+
+#### FORKING - quicker?
+
+source('~/Documents/LTBI_MDR/code/run_cohort_ltbi_mdr_inf_priors_parallel.R', echo=TRUE)
+
+save1 <- mclapply(1:10, run_cohort_parallel,mc.cores = no_cores)
