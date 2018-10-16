3e4088c112c6858b6ceaf2b83b0e1bae6ae606e5
GwenKn <g.knight@imperial.ac.uk>
2018-10-16 21:25:24 +0100

    recent infection calcs

--- code/recent_infection.R
+++ code/recent_infection.R
@@ -22,8 +22,6 @@ for(i in 1:llu){
     new_ds_age <- c(new_ds_age, 
                     as.numeric(c(c_ds_age[((ij-1)*20 + 1):((ij-1)*20+16)], 
                                mean(as.numeric(c_ds_age[((ij-1)*20+17):((ij-1)*20+20)])))))
-    
-    
   }
   c_dr_age <- colMeans(matrix(s[,"new_inf_r"],ncol = 40000/5))
   new_dr_age <- c()
@@ -36,6 +34,7 @@ for(i in 1:llu){
   # population size
   pop <- as.data.frame(POP2014[which(as.character(POP2014$iso3) == as.character(cni[i])),"value"])
   
+  if(length(new_dr_age) != 6800){print(c(i,length(new_dr_age)))}
   # MDR REP
   recinf <- as.data.frame(cbind(new_dr_age, new_ds_age))
   recinf$age_cat <- rep(seq(1,17,1), 400)
@@ -52,82 +51,114 @@ for(i in 1:llu){
   recinf$pop_name <- cni[i]
   
   ## store
-  if(i == 1){recinf_all <- recinf}
-  recinf_all <- rbind(recinf_all, recinf)
+  if(i == 1){recinf_all <- recinf}else{
+  recinf_all <- rbind(recinf_all, recinf)}
 }
 
+# dim(recinf_all) # 138*200*2*17
 recinf_all <- as.data.frame(recinf_all)
-rr <- recinf_all %>% group_by(pop_name, mdr_rep) 
-
-
-
-
-
-
-
-
-  s$kids <- 0
-  s[which(s$age < 15),"kids"] <- 1
-  
-  # aggregate by kids
-  s_k <- s %>% group_by(year,cn,mdr_rep,kids) %>% 
-    summarise_at(c("pr_dr","pr_ds","new_ds","new_dr","rei_sr","rei_rs"),funs(sum))
-  # aggregate by all
-  s_a <- s %>% group_by(year,cn,mdr_rep) %>% 
-    summarise_at(c("pr_dr","pr_ds","new_ds","new_dr","rei_sr","rei_rs"),funs(sum))
-  
-  
-  # store
-  if(i == 1){s_all <- s_a; s_kids <- s_k; s_orig <- s
-  }else{
-    s_all <- rbind(s_all, s_a)
-    s_kids <- rbind(s_kids, s_k)
-    s_orig <- rbind(s_orig,s)}
-  
-}
-
-
-
-s_all <- as.data.frame(s_all)
-colnames(s_all) <- colnames((s_a))
-
-s_kids <- as.data.frame(s_kids)
-colnames(s_kids) <- colnames((s_k))
-
-#s
-s_all$sum_new_s <- (as.numeric(s_all$new_ds) + as.numeric(s_all$rei_rs))
-s_kids$sum_new_s <- (as.numeric(s_kids$new_ds) + as.numeric(s_kids$rei_rs))
-
-s_all$prop_new_s <- (as.numeric(s_all$new_ds) + as.numeric(s_all$rei_rs))/as.numeric(s_all$pr_ds)
-s_kids$prop_new_s <- (as.numeric(s_kids$new_ds) + as.numeric(s_kids$rei_rs))/as.numeric(s_kids$pr_ds)
-
-#r
-s_all$sum_new_r <- (as.numeric(s_all$new_dr) + as.numeric(s_all$rei_sr))
-s_kids$sum_new_r <- (as.numeric(s_kids$new_dr) + as.numeric(s_kids$rei_sr))
-
-s_all$prop_new_r <- (as.numeric(s_all$new_dr) + as.numeric(s_all$rei_sr))/as.numeric(s_all$pr_dr)
-s_kids$prop_new_r <- (as.numeric(s_kids$new_dr) + as.numeric(s_kids$rei_sr))/as.numeric(s_kids$pr_dr)
-
-s_allm <- s_all %>% group_by(cn, mdr_rep) %>% summarise_at(c("sum_new_s","sum_new_r"), funs(sum))
+rr <- recinf_all %>% group_by(pop_name, mdr_rep,total_pop) %>% summarise_at(c("size_dr","size_ds"),funs(sum))
 
 # functions
 ub <- function(x)quantile(x,probs = .975, na.rm = TRUE)
 lb <- function(x)quantile(x,probs = .025, na.rm = TRUE)
 
-med.s_allm <- aggregate(s_allm[,c("sum_new_s","sum_new_r")],list(s_allm$cn), median, na.rm = TRUE)
-lb.s_allm <- aggregate(s_allm[,c("sum_new_s","sum_new_r")],list(s_allm$cn), lb)
-ub.s_allm <- aggregate(s_allm[,c("sum_new_s","sum_new_r")],list(s_allm$cn), ub)
+med.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), median, na.rm = TRUE)
+lb.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), lb)
+ub.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), ub)
 
 ## WHO levels
 load('~/Documents/LTBI_MDR/data/whokey.Rdata') # WHOkey has global region and iso3 codes
-s_allm$iso3 <- s_allm$cn
-s_allm.g <- merge(s_allm,WHOkey[,c('iso3','g_whoregion')],by='iso3',all.x=TRUE)
-med.s_allm.g <- aggregate(s_allm.g[,c("sum_new_s","sum_new_r")],list(s_allm.g$g_whoregion), median, na.rm = TRUE)
-lb.s_allm.g <- aggregate(s_allm.g[,c("sum_new_s","sum_new_r")],list(s_allm.g$g_whoregion), lb)
-ub.s_allm.g <- aggregate(s_allm.g[,c("sum_new_s","sum_new_r")],list(s_allm.g$g_whoregion), ub)
+rr$iso3 <- rr$pop_name
+rr.g <- merge(rr,WHOkey[,c('iso3','g_whoregion')],by='iso3',all.x=TRUE)
+med.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), median, na.rm = TRUE)
+lb.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), lb)
+ub.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), ub)
+
+## Total
+rr_total <-aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$mdr_rep), sum)
+med.rr.total<-colwise(median)(rr_total[,c("size_ds","size_dr")])
+lb.rr.total<-colwise(lb)(rr_total[,c("size_ds","size_dr")])
+ub.rr.total<-colwise(ub)(rr_total[,c("size_ds","size_dr")])
 
 
-s_allm_total <-aggregate(s_allm.g[,c("sum_new_s","sum_new_r")],list(s_allm.g$mdr_rep), sum)
-med.s_all_m.total<-colwise(median)(s_allm_total[,c("sum_new_s","sum_new_r")])
-lb.s_all_m.total<-colwise(lb)(s_allm_total[,c("sum_new_s","sum_new_r")])
-ub.s_all_m.total<-colwise(ub)(s_allm_total[,c("sum_new_s","sum_new_r")])
+### Percentage of global burden with recent LTBI
+global_pop <- sum(rr$total_pop) / 2 / 100 # 6893893 thousand # of these 138 countries 
+
+
+# ALL 
+paste0(sprintf('%.1f',100 * sum(med.rr.total) / global_pop), " [", 
+       sprintf('%.1f',100 * sum(lb.rr.total) / global_pop), "-", 
+       sprintf('%.1f',100 * sum(ub.rr.total) / global_pop),"]")
+
+# DS
+paste0(sprintf('%.1f',100 * med.rr.total$size_ds / global_pop), " [", 
+       sprintf('%.1f',100 * lb.rr.total$size_ds / global_pop), "-", 
+       sprintf('%.1f',100 * ub.rr.total$size_ds / global_pop),"]")
+# MDR
+paste0(sprintf('%.2f',100 * med.rr.total$size_dr / global_pop), " [", 
+       sprintf('%.2f',100 * lb.rr.total$size_dr / global_pop), "-", 
+       sprintf('%.2f',100 * ub.rr.total$size_dr / global_pop),"]")
+
+paste0(signif(med.rr.total$size_dr,2), " [", 
+       signif(lb.rr.total$size_dr,2), "-", 
+               signif(ub.rr.total$size_dr,2),"]")
+
+
+## Percentage MDR
+100*med.rr.total$size_dr / (med.rr.total$size_ds + med.rr.total$size_dr)
+
+####**** KIDS ****############
+recinf_all_kids <- recinf_all[which(recinf_all$age_cat < 4),]
+rr <- recinf_all_kids %>% group_by(pop_name, mdr_rep,total_pop) %>% summarise_at(c("size_dr","size_ds"),funs(sum))
+
+# dim(recinf_all_kids) # 138*3*2*200
+global_pop_kids <- sum(recinf_all_kids$pop) / (2*200)  # ~ 25% of population are < 15yo
+
+med.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), median, na.rm = TRUE)
+lb.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), lb)
+ub.rr <- aggregate(rr[,c("size_ds","size_dr")],list(rr$pop_name), ub)
+
+## WHO levels
+load('~/Documents/LTBI_MDR/data/whokey.Rdata') # WHOkey has global region and iso3 codes
+rr$iso3 <- rr$pop_name
+rr.g <- merge(rr,WHOkey[,c('iso3','g_whoregion')],by='iso3',all.x=TRUE)
+med.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), median, na.rm = TRUE)
+lb.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), lb)
+ub.rr.g <- aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$g_whoregion), ub)
+
+## Total
+rr_total <-aggregate(rr.g[,c("size_ds","size_dr")],list(rr.g$mdr_rep), sum)
+rr_total$perc_r <- 100*rr_total$size_dr / (rr_total$size_dr + rr_total$size_ds)
+
+med.rr.total<-colwise(median)(rr_total[,c("size_ds","size_dr","perc_r")])
+lb.rr.total<-colwise(lb)(rr_total[,c("size_ds","size_dr","perc_r")])
+ub.rr.total<-colwise(ub)(rr_total[,c("size_ds","size_dr","perc_r")])
+
+
+### Percentage of global burden with recent LTBI
+global_pop <- sum(rr$total_pop) / 2 / 100 # 6893893 thousand # of these 138 countries 
+
+
+# ALL 
+paste0(sprintf('%.1f',100 * sum(med.rr.total) / global_pop), " [", 
+       sprintf('%.1f',100 * sum(lb.rr.total) / global_pop), "-", 
+       sprintf('%.1f',100 * sum(ub.rr.total) / global_pop),"]")
+
+# DS
+paste0(sprintf('%.1f',100 * med.rr.total$size_ds / global_pop), " [", 
+       sprintf('%.1f',100 * lb.rr.total$size_ds / global_pop), "-", 
+       sprintf('%.1f',100 * ub.rr.total$size_ds / global_pop),"]")
+# MDR
+paste0(sprintf('%.2f',100 * med.rr.total$size_dr / global_pop), " [", 
+       sprintf('%.2f',100 * lb.rr.total$size_dr / global_pop), "-", 
+       sprintf('%.2f',100 * ub.rr.total$size_dr / global_pop),"]")
+
+paste0(signif(med.rr.total$size_dr,2), " [", 
+       signif(lb.rr.total$size_dr,2), "-", 
+       signif(ub.rr.total$size_dr,2),"]")
+
+
+## Percentage MDR
+100*med.rr.total$size_dr / (med.rr.total$size_ds + med.rr.total$size_dr)
+
