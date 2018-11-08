40a4617d0fb846a944344058097fffe5702c16b4
GwenKnight <gwen.knight@github.com>
2018-11-08 13:33:25 +0000

    metric update

--- code/data_against_need_infor_prior.R
+++ code/data_against_need_infor_prior.R
@@ -50,21 +50,21 @@ for(i in 1:length(countries)){
   d <-data[which(data$iso3 == cnn),]
   d$type <- "MDR"
 
-  # ggplot() +
-  #   geom_rect( data = p[which(p$mdr_rep < 9),] ,
-  #              mapping = aes( xmin = lowery ,
-  #                             xmax = uppery ,
-  #                             ymin = 0 ,
-  #                             ymax = 50 ,
-  #                             fill = level_pr_ltbir) ,
-  #              alpha = .5 ) + facet_wrap(~mdr_rep, ncol = 4) +
-  #   scale_fill_brewer(name = "Level of\ncontribution\nto MDR LTBI",drop = FALSE,direction = -1,palette = "Spectral",
-  #                     labels = c("0","(0-10%]","(10-20%]","(20-30%]","(30-40%]","(40-50%]","(50-60%]",
-  #                                "(60-70%]","(70-80%]","(80-90%]","(90-100%]")) +
-  #   scale_x_continuous("Year") + scale_y_continuous("Percentage of new cases that are MDR") +
-  #   geom_point( data = d , aes(x=year_new, y = 100*new_mdr_prop, group = iso3)) +
-  #   geom_errorbar(data = d, aes(x= year_new, ymax = 100*mhi, ymin = 100*mlo))
-  # ggsave(paste0("~/Dropbox/MDR/output/eg_when_imp_vs_data_",cnn,"_",nari,"_",labl,".pdf"),width = 12, height = 4)
+  ggplot() +
+    geom_rect( data = p[which(p$mdr_rep < 9),] ,
+               mapping = aes( xmin = lowery ,
+                              xmax = uppery ,
+                              ymin = 0 ,
+                              ymax = 50 ,
+                              fill = level_pr_ltbir) ,
+               alpha = .5 ) + facet_wrap(~mdr_rep, ncol = 4) +
+    scale_fill_brewer(name = "Level of\ncontribution\nto MDR LTBI",drop = FALSE,direction = -1,palette = "Spectral",
+                      labels = c("0","(0-10%]","(10-20%]","(20-30%]","(30-40%]","(40-50%]","(50-60%]",
+                                 "(60-70%]","(70-80%]","(80-90%]","(90-100%]")) +
+    scale_x_continuous("Year") + scale_y_continuous("Percentage of new cases that are MDR") +
+    geom_point( data = d , aes(x=year_new, y = 100*new_mdr_prop, group = iso3)) +
+    geom_errorbar(data = d, aes(x= year_new, ymax = 100*mhi, ymin = 100*mlo))
+  ggsave(paste0("~/Dropbox/MDR/output/eg_when_imp_vs_data_",cnn,"_",nari,"_",labl,".pdf"),width = 12, height = 4)
 
   ### Mean over mdr_rep
   mean_pr <- pltbir %>% group_by(yearcat,lowery,uppery) %>%
@@ -79,27 +79,27 @@ for(i in 1:length(countries)){
 
   mean_both$level_pr_ltbi <- cut(mean_both$mean,seq(-0.1,1,0.1),labels=seq(0,100,10),right = TRUE)
 
-  # ggplot() +
-  #   geom_rect( data = mean_both ,
-  #              mapping = aes( xmin = lowery ,
-  #                             xmax = uppery ,
-  #                             ymin = 0 ,
-  #                             ymax = 50 ,
-  #                             fill = level_pr_ltbi) ,
-  #              alpha = .5 ) + facet_wrap(~type, ncol = 2) +
-  #   scale_fill_brewer(name = "Level of\ncontribution\nto MDR LTBI",drop = FALSE,direction = -1,palette = "Spectral",
-  #                     labels = c("0","(0-10%]","(10-20%]","(20-30%]","(30-40%]","(40-50%]","(50-60%]",
-  #                                "(60-70%]","(70-80%]","(80-90%]","(90-100%]")) +
-  #   scale_x_continuous("Year") + scale_y_continuous("Percentage of new cases that are MDR") +
-  #   geom_point( data = d , aes(x=year_new, y = 100*new_mdr_prop, group = iso3)) +
-  #   geom_errorbar(data = d, aes(x= year_new, ymax = 100*mhi, ymin = 100*mlo))
-  # ggsave(paste0("~/Dropbox/MDR/output/when_imp_vs_data_mean_",cnn,"_",nari,"_",labl,".pdf"),width = 12, height = 4)
+  ggplot() +
+    geom_rect( data = mean_both ,
+               mapping = aes( xmin = lowery ,
+                              xmax = uppery ,
+                              ymin = 0 ,
+                              ymax = 50 ,
+                              fill = level_pr_ltbi) ,
+               alpha = .5 ) + facet_wrap(~type, ncol = 2) +
+    scale_fill_brewer(name = "Level of\ncontribution\nto MDR LTBI",drop = FALSE,direction = -1,palette = "Spectral",
+                      labels = c("0","(0-10%]","(10-20%]","(20-30%]","(30-40%]","(40-50%]","(50-60%]",
+                                 "(60-70%]","(70-80%]","(80-90%]","(90-100%]")) +
+    scale_x_continuous("Year") + scale_y_continuous("Percentage of new cases that are MDR") +
+    geom_point( data = d , aes(x=year_new, y = 100*new_mdr_prop, group = iso3)) +
+    geom_errorbar(data = d, aes(x= year_new, ymax = 100*mhi, ymin = 100*mlo))
+  ggsave(paste0("~/Dropbox/MDR/output/when_imp_vs_data_mean_",cnn,"_",nari,"_",labl,".pdf"),width = 12, height = 4)
 
 
   ## METRIC
   ## sum over 5 yrs % LTBI from that 5 yr x data there (y/n) = M. High = good low = ba
   ## table of those with data in that year group (0 if none, 1 if some) * proportion of ltbi in there
-  p$metric <-   rep(ifelse(table(d$cut_year_new)>0,1,0),length(unique(p$mdr_rep))) * as.numeric(p$sum_prltbir)
+  p$metric <-   rep(ifelse(table(d$cut_year_new)>0,1,0),each = length(unique(p$mdr_rep))) * as.numeric(p$sum_prltbir)
   p <- as.data.table(p)
   ## Sum Metric = proportion of LTBI that could be gotten at using this data
   pp <- ddply(p, .(mdr_rep), summarize,  sum_metric=sum(metric)) #pp <- p %>% group_by(mdr_rep) %>% summarise(sum_metric = sum(metric))
@@ -112,6 +112,10 @@ store_metric <- as.data.frame(store_metric)
 colnames(store_metric) <- c("mdr_rep","sum_metric","cnn","model")
 write.csv(store_metric, paste0("~/Dropbox/MDR/output/store_metric_",nari,".csv"))
 
+######*****#######
+######*****#######
+######*****#######
+######*****#######
 # READ in if need
 store_metric <- read.csv(paste0("~/Dropbox/MDR/output/store_metric_",nari,".csv"))[,-1]
 
@@ -132,18 +136,27 @@ mdr30 <- read.csv("~/Dropbox/MRC SD Fellowship/Research/MDR/WHO_data/top30_mdr_c
 
 # add in mean
 store_metric$mean_s <- 0
+store_median <- c()
 for(i in 1:138){w<-which(as.character(store_metric$cnn) == as.character(countries[i]));
-mean_s <- median(store_metric[w,"sum_metric"])
+mean_s <- median(store_metric[w,"sum_metric"]);
+store_median <- c(store_median,mean_s);
 store_metric[w,"mean_s"] <- mean_s}
+
+# medians for each country
+store_median <- cbind(store_median, countries)
+
 
 w<-c()
 for(i in 1:30){w<-c(w,which(as.character(store_metric$cnn) == as.character(mdr30[i,"iso3"])))}
+theme_set(theme_bw())
 
 ggplot(store_metric[w,], aes(x=cnn, y = sum_metric )) + geom_boxplot(outlier.shape = NA) + 
   scale_x_discrete("Country") + scale_y_continuous("Metric for MDR-LTBI data coverage") +
   theme(axis.text.y = element_text(size = 10)) + 
   coord_flip() + aes(x=reorder(cnn,sum_metric),y=sum_metric) 
 ggsave(paste0("~/Dropbox/MDR/output/MDR_metric_data_against_need_",nari,"_",labl,"_top30.pdf"), width = 14, height = 14)
+
+store_metric[w,]
 
 ## Single country
 w<- which(store_metric$cnn == "BWA")
@@ -165,6 +178,7 @@ store_metric_med <- as.data.frame(store_metric) %>% group_by(cnn, model) %>% dpl
 # define color buckets
 store_metric_med$metric <- as.numeric(cut(store_metric_med$med, c(0, 0.1,0.2,0.3,0.4,0.5,0.6,0.7, 0.8, 0.9, 1)))
 store_metric_med <- as.data.frame(store_metric_med)
+cols <- colorRampPalette(brewer.pal(11,"Reds"), bias = 2)(13)
 
 mapped_data <- joinCountryData2Map(store_metric_med, joinCode = "ISO3", nameJoinColumn = "cnn")
 
